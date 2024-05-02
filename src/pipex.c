9f80b6603745420188c8515fb6f759c7c9c2ef1f
Ruben Gallien <rgallien@bess-f2r1s9.clusters.42paris.fr>
2024-05-02 15:31:48 +0200

    ok?

--- src/pipex.c
+++ src/pipex.c
@@ -1,8 +1,23 @@
 #include "pipex.h"
 
-int main(int ac, char *av[])
+int main(int ac, char *argv[])
 {
-	if (ac == 2)
-		ft_printf("%s\n", av[1]);
+	int	i;
+	int	fd;
+
+	i = 0;
+	fd = open("example.txt", O_WRONLY | O_CREAT, 0644);
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	if (ac > 0)
+	{
+		printf("nb arg : %d\n", ac);
+		while (i < ac)
+		{
+			printf("%s\n",argv[i]);
+			i++;
+		}
+	}
+
 	return (0);
 }
