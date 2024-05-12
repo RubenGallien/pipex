abc625e17c8de3071a7b7e1f3b1d0c61748e10bf
rgallien <rgallien@student.42.fr>
2024-05-12 19:43:59 +0200

    here_doc

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: rgallien <rgallien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 11:39:14 by rgallien          #+#    #+#             */
-/*   Updated: 2024/05/08 12:06:15 by rgallien         ###   ########.fr       */
+/*   Updated: 2024/05/12 18:47:33 by rgallien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <sys/wait.h>
+
+typedef struct s_pipex
+{
+	int		in_out_fd[2];
+	int		here_doc;
+	int		is_invalid_infile;
+	char	**cmd_paths;
+	char	***cmd_args;
+	int		cmd_count;
+}	t_pipex;
 
 char	*find_env(char **envp);
 void	ft_free_tab(char **tab);
--- src/pipex.c
+++ src/pipex.c
@@ -6,28 +6,35 @@
 /*   By: rgallien <rgallien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 11:37:38 by rgallien          #+#    #+#             */
-/*   Updated: 2024/05/08 12:24:45 by rgallien         ###   ########.fr       */
+/*   Updated: 2024/05/12 19:36:39 by rgallien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	ft_here_doc(char *end)
+{
+	char *str;
+
+	str = get_next_line(0);
+	while (str && ft_strncmp(end, str, ft_strlen(str) - 1) != 0)
+	{
+		free(str);
+		str = get_next_line(0);
+	}
+	free(str);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	int		pipeline;
 	char	**all_path;
-	int		i;
-
-	i = 0;
-	if (!envp[0])
-		return (-1);
+	if (ac < 5)
+		return (0);
+	if (ft_strncmp("here_doc", av[1], 9) == 0)
+		ft_here_doc(av[2]);
 	pipeline = ac - 4;
 	all_path = ft_split(find_env(envp) + ft_strlen("PATH="), ':');
-	while (all_path[i])
-	{
-		ft_printf("%s\n", all_path[i]);
-		i++;
-	}
-	ft_printf("nb_args = %d\n%s\n", pipeline, av[0]);
+	printf("%d\n", pipeline);
 	return (ft_free_tab(all_path), 0);
 }
